cf3229dd8fe5cdc5104e97276b12a568597d26e3
Elroy Kanye <elroykanye@gmail.com>
2021-09-28 12:41:09 +0100

    added base auth service with firebase client

---
+++ keyline/keyline/Service/UserService.cs
@@ -0,0 +1,60 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using keyline.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keyline.Service
+{
+    class UserService
+    {
+        FirebaseClient firebaseClient;
+        private string databaseEndpoint = "https://keyline-17e32-default-rtb.firebaseio.com/"
+
+        public UserService()
+        {
+            firebaseClient = new FirebaseClient(databaseEndpoint);
+        }
+
+        public async Task<bool> IsUserExists(string username)
+        {
+            var user = (await firebaseClient.Child("Users")
+                .OnceAsync<User>()).Where(u => u.Object.Username.Equals(username))
+                .FirstOrDefault();
+
+            return user != null;
+        }
+
+        public async Task<bool> RegisterUser(string username, string password)
+        {
+            if (await IsUserExists(username) == false)
+            {
+                await firebaseClient.Child("Users")
+                    .PostAsync(new User()
+                    {
+                        Username = username,
+                        Password = password
+                    });
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> LoginUser(string username, string password)
+        {
+            var user = (await firebaseClient.Child("Users")
+                .OnceAsync<User>())
+                .Where(u => u.Object.Username == username)
+                .Where(u => u.Object.Password == password)
+                .FirstOrDefault();
+
+            return user != null; 
+        }
+    }
+}
