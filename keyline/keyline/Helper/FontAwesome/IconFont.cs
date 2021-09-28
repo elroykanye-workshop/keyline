95a3f7aac92cd7cbb3c707a0512c1ed90e54a16c
Elroy Kanye <elroykanye@gmail.com>
2021-09-28 18:03:09 +0100

    font awesome cs support

---
+++ keyline/keyline/Helper/FontAwesome/IconFont.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace keyline.Helper
+{
+    class IconFont
+    {
+        public static readonly string ViewList = "list-ul";
+        public static readonly string Shopping = "shopping-cart";
+        public static readonly string SearchWeb = "fas fa-search";
+    }
+}
