afe1f806ab089e0202758742d35146db79d5fec2
Elroy Kanye <elroykanye@gmail.com>
2021-09-28 12:41:59 +0100

    package config changes

--- keyline/keyline/App.xaml.cs
+++ keyline/keyline/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using keyline.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,8 @@ namespace keyline
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
+            MainPage = new LoginView();
         }
 
         protected override void OnStart()
